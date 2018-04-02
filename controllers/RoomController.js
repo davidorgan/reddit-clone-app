837fc0b1e18433bb41640e44c134d6ce47555ce9
davidorgan <david.sorgan@gmail.com>
2018-04-01 20:43:23 -0500

    Setting up git for reddit clone
    Setting up git for reddit clone

---
+++ controllers/RoomController.js
@@ -0,0 +1,49 @@
+const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
+const Promise = require('bluebird')
+const resource = 'room'
+
+module.exports = {
+    get: (params) => {
+        return new Promise((resolve, reject) =>{
+            turbo.fetch(resource, params)
+            .then(data => {
+                resolve(data)
+            })
+            .catch(err => {
+                reject(err)
+            })
+
+        })       
+    },
+    getById: (id) => {
+        return new Promise((resolve, reject) =>{
+            turbo.fetchOne(resource, id)
+            .then(data => {
+                resolve(data)
+            })
+            .catch(err => {
+                reject(err)
+            })
+        })    
+    },
+    post: (body) => {
+        return new Promise((resolve, reject) =>{
+            body['subscribers'] = [body.user]         
+            turbo.fetchOne('user', body.user)
+            .then(user => {
+                body['user'] = {
+                    id: user.id,
+                    username: user.username,
+                    image: user.image
+                }
+                return turbo.create(resource, body)
+            })
+            .then(data => {
+                resolve(data)
+            })
+            .catch(err => {
+                reject(err)
+            })
+        })    
+    }
+}
---
+++ routes/api.js
@@ -0,0 +1,92 @@
+// Full Documentation - https://www.turbo360.co/docs
+const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
+const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
+const router = vertex.router()
+const controllers = require('../controllers')
+
+router.post('/:resource', (req, res) => {
+
+	const resource = req.params.resource
+	const controller = controllers[resource]
+	if(controller == null){
+		res.json({
+			confirmation: "fail",
+			message: "Invalid resource: " + resource
+		})
+		return
+	}
+
+	controller.post(req.body)
+	.then(data => {
+		res.json({
+			confirmation: 'success',
+			data: data
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: errr.message
+		})
+	})
+
+})
+
+
+router.get('/:resource', (req, res) => {
+
+	const resource = req.params.resource
+	const controller = controllers[resource]
+	if(controller == null){
+		res.json({
+			confirmation: "fail",
+			message: "Invalid resource: " + resource
+		})
+		return
+	}
+
+	controller.get(req.query)
+	.then(data =>{
+		res.json({
+			confirmation: "success",
+			data: data
+		})
+	})
+	.catch(err =>{
+		res.json({
+			confirmation: "fail",
+			data: err.message
+		})
+	})
+})
+
+router.get('/:resource/:id', (req, res) => {
+
+	const resource = req.params.resource
+	const controller = controllers[resource]
+	if(controller == null){
+		res.json({
+			confirmation: "fail",
+			message: "Invalid resource: " + resource
+		})
+		return
+	}
+
+	controller.getById(req.params.id)
+	.then(data =>{
+		res.json({
+			confirmation: "success",
+			data: data
+		})
+	})
+	.catch(err =>{
+		res.json({
+			confirmation: "fail",
+			data: err.message
+		})
+	})
+})
+
+
+
+module.exports = router
